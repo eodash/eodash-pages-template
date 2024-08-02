54b3b1f2caca8e3f5123971054f782a790ce630d
Daniel Santillan <daniel.santillan@eox.at>
2024-08-02 10:48:07 +0200

    chore: updating eodash package and configuration files accordingly

--- public/configs/firstconfig.js
+++ public/configs/firstconfig.js
@@ -1,50 +1,78 @@
 export default {
-    id: 'app',
-    stacEndpoint: "https://esa-eodash.github.io/RACE-catalog/RACE/catalog.json",
-    brand: {
-        noLayout:true,
-        name: 'runtime config 1',
-        font: { family: "Roboto" },
-        theme: {
-            colors: {
-                primary: "#AE9537",
-                surface: "#f2f4f3",
-                secondary: "#AE9537"
-            }
-        }
+  id: "app",
+  stacEndpoint:
+    "https://eodashcatalog.eox.at/test-style/trilateral/catalog.json",
+  brand: {
+    noLayout: true,
+    name: "runtime config 1",
+    font: { family: "Roboto" },
+    theme: {
+      colors: {
+        primary: "#AE9537",
+        surface: "#f2f4f3",
+        secondary: "#AE9537",
+      },
     },
-    template: {
-        loading: {
-            id: Symbol(),
-            type: "web-component",
-            widget: {
-                link: "https://cdn.jsdelivr.net/npm/ldrs/dist/auto/mirage.js",
-                tagName: "l-mirage",
-                properties: {
-                    class: "align-self-center justify-self-center",
-                    size: "120",
-                    speed: "2.5",
-                    color: "#004170"
-                }
-            }
+  },
+  template: {
+    loading: {
+      id: Symbol(),
+      type: "web-component",
+      widget: {
+        link: "https://cdn.jsdelivr.net/npm/ldrs/dist/auto/mirage.js",
+        tagName: "l-mirage",
+        properties: {
+          class: "align-self-center justify-self-center",
+          size: "120",
+          speed: "2.5",
+          color: "#004170",
         },
-        background: {
-            id: Symbol(),
-            type: "internal",
-            widget: {
-                name: "EodashMap"
-            }
+      },
+    },
+    background: {
+      id: Symbol(),
+      type: "internal",
+      widget: {
+        name: "EodashMap",
+        properties: {
+          enableCompare: true,
         },
-        widgets: [
-            {
-                layout: { x: 0, y: 0, w: 3, h: 12 },
-                title: "Tools",
-                id: Symbol(),
-                type: "internal",
-                widget: {
-                    name: "EodashItemFilter"
-                }
-            }
-        ]
-    }
-}
+      },
+    },
+    widgets: [
+      {
+        layout: { x: 0, y: 0, w: 3, h: 12 },
+        title: "Tools",
+        id: Symbol(),
+        type: "internal",
+        widget: {
+          name: "EodashItemFilter",
+        },
+        properties: {
+          enableCompare: true,
+          aggregateResults: "themes",
+        },
+      },
+      {
+        id: "Information",
+        title: "Information",
+        layout: { x: 9, y: 0, w: 3, h: 12 },
+        type: "web-component",
+        widget: {
+          link: "https://cdn.jsdelivr.net/npm/@eox/stacinfo",
+          properties: {
+            for: window.eodashStore.states.currentUrl,
+            allowHtml: "true",
+            header: '["title"]',
+            tags: '["themes"]',
+            subheader: "[]",
+            properties: '["satellite","sensor","agency","extent"]',
+            featured: '["description","providers","assets","links"]',
+            footer: '["sci:citation"]',
+          },
+          tagName: "eox-stacinfo",
+        },
+      },
+    ],
+  },
+};
--- public/configs/secondconfig.js
+++ public/configs/secondconfig.js
@@ -1,59 +1,69 @@
 export default {
-    id: 'app',
-    stacEndpoint: "https://eurodatacube.github.io/eodash-catalog/trilateral/catalog.json",
-    brand: {
-        noLayout:true,
-        name: 'runtime config 2',
-        font: { family: "Noto Sans" },
-        theme: {
-            colors: {
-                primary: "#AE9537",
-                surface: "#f2f4f3",
-                secondary: "#AE9537"
-            }
-        }
+  id: "app",
+  stacEndpoint:
+    "https://eodashcatalog.eox.at/test-style/trilateral/catalog.json",
+  brand: {
+    noLayout: true,
+    name: "runtime config 2",
+    font: { family: "Noto Sans" },
+    theme: {
+      colors: {
+        primary: "#AE9537",
+        surface: "#f2f4f3",
+        secondary: "#AE9537",
+      },
     },
-    template: {
-        loading: {
-            id: Symbol(),
-            type: "web-component",
-            widget: {
-                link: "https://cdn.jsdelivr.net/npm/ldrs/dist/auto/mirage.js",
-                tagName: "l-mirage",
-                properties: {
-                    class: "align-self-center justify-self-center",
-                    size: "120",
-                    speed: "2.5",
-                    color: "#a34b20"
-                }
-            }
+  },
+  template: {
+    loading: {
+      id: Symbol(),
+      type: "web-component",
+      widget: {
+        link: "https://cdn.jsdelivr.net/npm/ldrs/dist/auto/mirage.js",
+        tagName: "l-mirage",
+        properties: {
+          class: "align-self-center justify-self-center",
+          size: "120",
+          speed: "2.5",
+          color: "#a34b20",
         },
-        background: {
-            id: Symbol(),
-            type: "internal",
-            widget: {
-                name: "EodashMap"
-            }
+      },
+    },
+    background: {
+      id: Symbol(),
+      type: "internal",
+      widget: {
+        name: "EodashMap",
+      },
+    },
+    widgets: [
+      {
+        layout: { x: 0, y: 0, w: 3, h: 12 },
+        title: "Tools",
+        id: Symbol(),
+        type: "internal",
+        widget: {
+          name: "EodashItemFilter",
         },
-        widgets: [
-            {
-                layout: { x: 0, y: 0, w: 3, h: 12 },
-                title: "Tools",
-                id: Symbol(),
-                type: "internal",
-                widget: {
-                    name: "EodashItemFilter"
-                }
-            },
-            {
-                layout: { x: 4, y: 0, w: 4, h: 4 },
-                title: "Date Picker",
-                id: Symbol(),
-                type: "internal",
-                widget: {
-                    name: "EodashDatePicker",
-                }
-            }
-        ]
-    }
-}
+      },
+      {
+        layout: { x: 5, y: 10, w: 1, h: 1 },
+        title: "Date Picker",
+        id: Symbol(),
+        type: "internal",
+        widget: {
+          name: "EodashDatePicker",
+        },
+      },
+      {
+        id: Symbol(),
+        type: "internal",
+        title: "Layer Control",
+        layout: { x: 9, y: 0, w: 3, h: 12 },
+        widget: {
+          name: "EodashLayerControl",
+        },
+      },
+    ],
+  },
+};
