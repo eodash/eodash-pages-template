69e85e961f52c89b0d96892ef81380ff7e03dc3f
lubojr <lubojr@users.noreply.github.com>
2026-04-16 15:52:13 +0000

    Remove preview for PR 3 🛬

--- pr-preview/pr-3/assets/chunks/ssrBoot-Zgc_Ttvi.CZl8-OKR.js
+++
@@ -1,1 +0,0 @@
-import{v as t,S as e,G as s,aq as n}from"./framework.CRIhza1-.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:n(()=>o.value?void 0:{transition:"none !important"}),isBooted:e(o)}}export{i as u};
